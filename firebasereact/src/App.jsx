872f2e81775e87003e134b6f9807db743ffc6975
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-11-24 18:56:53 -0300

    base logic

--- firebasereact/src/App.jsx
+++ firebasereact/src/App.jsx
@@ -1,3 +1,45 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+import { useState, useEffect } from "react";
+
+
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyDNJOeSD4E_yAGRnUnur_9gXxynu8lsfEg",
+  authDomain: "reactfirebase-f2980.firebaseapp.com",
+  projectId: "reactfirebase-f2980",
+});
+
+
 export const App = () => {
-  return <h1>Hello world</h1>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [users, setUsers] = useState([]);
+
+  const db = getFirestore(firebaseApp);
+  const userCollectionRef = collection(db, "users");
+
+  useEffect(()=>{
+    const getUsers = async () =>{
+      const data = await getDocs(userCollectionRef)
+      setUsers(data.docs.map((doc)=>({...doc.data(),id: doc.id})))
+    };
+    getUsers();
+  },[])
+
+  return (
+    <div>
+      <ul>
+        {users.map(user => {
+          return(
+            <div key={user.id}>
+              <li>{user.name}</li>
+              <li>{user.email}</li>
+            </div>
+          )
+        })}
+      </ul>
+    </div>
+  )
 };
