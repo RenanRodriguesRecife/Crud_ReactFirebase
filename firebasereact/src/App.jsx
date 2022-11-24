fcae5bd4df0120b2024f7fe6d7add7f06257f1e1
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-11-24 19:16:04 -0300

    delete user

--- firebasereact/src/App.jsx
+++ firebasereact/src/App.jsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, getDocs, collection } from "firebase/firestore";
+import { getFirestore, getDocs, collection, addDoc, doc, deleteDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 
 
@@ -20,6 +20,19 @@ export const App = () => {
   const db = getFirestore(firebaseApp);
   const userCollectionRef = collection(db, "users");
 
+  async function criarUser(){
+    const user = await addDoc(userCollectionRef,{
+      name, email,
+    });
+    console.log(user)
+  }
+
+  async function deleteUser(id){
+    const useDoc = doc(db, 'users', id);
+    await deleteDoc(useDoc);
+  }
+
+ 
   useEffect(()=>{
     const getUsers = async () =>{
       const data = await getDocs(userCollectionRef)
@@ -30,12 +43,16 @@ export const App = () => {
 
   return (
     <div>
+      <input type="text" placeholder="Nome..."value={name} onChange={(e)=>setName(e.target.value)}/>
+      <input type="text" placeholder="email..." value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <button onClick={criarUser}>Criar user</button>
       <ul>
         {users.map(user => {
           return(
             <div key={user.id}>
               <li>{user.name}</li>
               <li>{user.email}</li>
+              <button onClick={()=>deleteUser(user.id)}>Deletar</button>
             </div>
           )
         })}
